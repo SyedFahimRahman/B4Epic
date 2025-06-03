2ef3a967a071f714ad315537c6211351c73b6654
waleed-ahmd <waleedansaari786@gmail.com>
2025-06-03 15:12:58 +0100

    updated waleed

--- config.py
+++ config.py
@@ -6,3 +6,11 @@ load_dotenv()
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    MAIL_SERVER = os.getenv("MAIL_SERVER", "sandbox.smtp.mailtrap.io")
+    MAIL_PORT = int(os.getenv("MAIL_PORT", 2525))
+    MAIL_USERNAME = os.getenv("MAIL_USERNAME")
+    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+    MAIL_USE_TLS = os.getenv("MAIL_USE_TLS", "True") == "True"
+    MAIL_USE_SSL = os.getenv("MAIL_USE_SSL", "False") == "True"
+    MAIL_DEFAULT_SENDER = os.getenv("MAIL_DEFAULT_SENDER", "7415f7e300a9c0")
